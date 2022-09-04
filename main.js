67bc88a26b5e113802b8d833ff32fbfb06c4e16e
tarekali <108217205+6tarek@users.noreply.github.com>
2022-09-04 05:50:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,82 +1,64 @@
-// Main Variables
-let theInput = document.querySelector(".get-repos input");
-let getButton = document.querySelector(".get-button");
-let reposData = document.querySelector(".show-data");
-
-getButton.onclick = function () {
-  getRepos();
-};
-
-// Get Repos Function
-function getRepos() {
-
-  if (theInput.value == "") { // If Value Is Empty
-
-    reposData.innerHTML = "<span>Please Write Github Username.</span>";
-
+///////////////  get document
+let serial = document.getElementById(`serial`);
+let create = document.getElementById(`generate`);
+let nun = document.getElementById("nn");
+let closee = document.getElementById("close");
+let oppen = document.getElementById("oppen");
+let bac = document.getElementById("bak");
+let dark = document.getElementById("dark");
+let auto = document.getElementById("auto");
+let random = document.getElementById("random");
+let navv = document.getElementById("navv");
+let nave = document.getElementById("nave");
+let color = document.getElementById("nave1");
+//////////////////////
+create.onclick = function () {
+  let pass =
+    "1234567980qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM?@#!~$&";
+  let nunn = nun.value;
+  let ran = "";
+  if (nunn >= "15") {
+    window.alert("pls enter yoer number > 15");
   } else {
-
-    fetch(`https://api.github.com/users/${theInput.value}/repos`)
-
-    .then((response) => response.json())
-
-    .then((repositories) => {
-
-      // Empty The Container
-      reposData.innerHTML = '';
-
-      // Loop On Repositories
-      repositories.forEach(repo => {
-
-        // Create The Main Div Element
-        let mainDiv = document.createElement("div");
-
-        // Create Repo Name Text
-        let repoName = document.createTextNode(repo.name);
-
-        // Append The Text To Main Div
-        mainDiv.appendChild(repoName);
-
-        // Create Repo URL Anchor
-        let theUrl = document.createElement('a');
-
-        // Create Repo Url Text
-        let theUrlText = document.createTextNode("Visit");
-
-        // Append The Repo Url Text To Anchor Tag
-        theUrl.appendChild(theUrlText);
-
-        // Add Thje Hypertext Reference "href"
-        theUrl.href = `https://github.com/${theInput.value}/${repo.name}`;
-
-        // Set Attribute Blank
-        theUrl.setAttribute('target', '_blank');
-
-        // Append Url Anchor To Main Div
-        mainDiv.appendChild(theUrl);
-
-        // Create Stars Count Span
-        let starsSpan = document.createElement("span");
-
-        // Create The Stars Count Text
-        let starsText = document.createTextNode(`Stars ${repo.stargazers_count}`);
-
-        // Add Stars Count Text To Stars Span
-        starsSpan.appendChild(starsText);
-
-        // Append Stars Count Span To Main Div
-        mainDiv.appendChild(starsSpan);
-
-        // Add Class On Main Div
-        mainDiv.className = 'repo-box';
-
-        // Append The Main Div To Container
-        reposData.appendChild(mainDiv);
-
-      });
-
-    });
-
+    for (let i = 0; i < nunn; i++) {
+      ran += pass[Math.floor(Math.random() * pass.length)];
+    }
+    serial.innerHTML = ran;
   }
+};
+//////////////////////random
+/*
+  Practice
+  - Random Background Color
+*/
+// #fd34f7 FF00FF
+random.onclick = function (){
+let hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let colorParts = [];
 
+for (let i = 0; i < 6; i++) {
+  colorParts.push(hexArray[Math.floor(Math.random() * hexArray.length)]);
+}
+let finalColor = `#${colorParts.join("")}`;
+document.body.style.backgroundColor = finalColor;
+}
+////////////////////////////////color inbut
+navv.onkeyup = function (){
+    if(navv.value > 7 )return
+    else{
+        document.body.style.backgroundColor = `${navv.value}`;
+  }
+}
+///////////////////dark
+dark.onclick= function(){
+    document.body.style.backgroundColor = `#000`
+}
+/////////////////////////////oppen
+oppen.onclick = function(){
+nave.classList.remove('remove')
+oppen.classList.add(`remove`)
+}
+/////////////////////////bac
+bac.onclick = function(){
+    color.classList.remove('remove')
 }
